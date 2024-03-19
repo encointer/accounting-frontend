b7263b8ab256cfb11995b1bd31d5ae993213ce0b
pifragile <pigu@bluewin.ch>
2024-03-19 09:40:27 +0800

    add 2024

--- src/components/CidYearForm.js
+++ src/components/CidYearForm.js
@@ -11,6 +11,7 @@ const CidYearForm = ({ handleSubmit }) => {
                     <div className="control">
                         <div className="select">
                             <select class="form-select" id="year" name="year">
+                                <option value="2024">2024</option>
                                 <option value="2023">2023</option>
                                 <option value="2022">2022</option>
                             </select>
