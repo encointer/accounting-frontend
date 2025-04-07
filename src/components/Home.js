12dd71071ff9f2ca5631f99a321b9c7ff359b62c
Alain Brenzikofer <alain@integritee.network>
2025-04-07 16:30:34 +0200

    add Paynuq overview

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,25 +3,30 @@ import RewardsChart from "./RewardsChart";
 import SankeyChart from "./SankeyChart";
 
 const Home = () => {
-    return (
-        <PublicLayout>
-            <br/>
-            <big>Leu Zürich Rewards Overview</big>
-            <br/>
-            <br/>
-            <RewardsChart cid="u0qj944rhWE"/>
-            <br/>
-            <big>Greenbay Dollar Rewards Overview</big>
-            <br/>
-            <br/>
-            <RewardsChart cid="dpcmj33LUs9"/>
-            <br/>
-            <big>Nyota Rewards Overview</big>
-            <br/>
-            <br/>
-            <RewardsChart cid="kygch5kVGq7"/>
-        </PublicLayout>
-    );
+  return (
+    <PublicLayout>
+      <br/>
+      <big>Leu Zürich Rewards Overview</big>
+      <br/>
+      <br/>
+      <RewardsChart cid="u0qj944rhWE"/>
+      <br/>
+      <big>Nyota Rewards Overview</big>
+      <br/>
+      <br/>
+      <RewardsChart cid="kygch5kVGq7"/>
+      <br/>
+      <big>Paynuq Rewards Overview</big>
+      <br/>
+      <br/>
+      <RewardsChart cid="s1vrqQL2SD"/>
+      <br/>
+      <big>Greenbay Dollar Rewards Overview</big>
+      <br/>
+      <br/>
+      <RewardsChart cid="dpcmj33LUs9"/>
+    </PublicLayout>
+  );
 };
 
 export default Home;
