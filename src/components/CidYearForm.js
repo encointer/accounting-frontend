4f524a20b8733a7f69bc1e0295af0b9e39ec6657
Alain Brenzikofer <alain@integritee.network>
2026-01-23 08:28:46 +0100

    add year 2026

--- src/components/CidYearForm.js
+++ src/components/CidYearForm.js
@@ -11,6 +11,7 @@ const CidYearForm = ({ handleSubmit }) => {
                     <div className="control">
                         <div className="select">
                             <select class="form-select" id="year" name="year">
+                                <option value="2026">2026</option>
                                 <option value="2025">2025</option>
                                 <option value="2024">2024</option>
                                 <option value="2023">2023</option>
